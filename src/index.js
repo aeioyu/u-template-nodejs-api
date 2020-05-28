ccb68af63d25ea35c222bf94d1117abfc17ffebd
siripong <aeio.yu@gmail.com>
2020-05-28 19:17:54 +0700

    add babel

--- src/index.js
+++ src/index.js
@@ -2,23 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import morgan from 'morgan';
+import compression from 'compression';
 import routes from './routes';
 
 const app = express();
-
+app.use(compression());
+app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const publicPath = path.resolve('build', 'public');
-app.use('/', express.static(publicPath, { maxAge: 31557600000 }));
-
 app.use('/api', routes);
+
+const publicPath = path.resolve('public');
+app.use('/', express.static(publicPath, { maxAge: 31557600000 }));
 
 app.all('*', (req, res) => {
   res.status(405).send({ error: true, message: 'Method Not Allowed' });
 });
 
-const server = app.listen(3000);
+const server = app.listen(3000, () => {
+  const serverAddress = server.address();
+
+  // eslint-disable-next-line no-console
+  console.log(`Server start at port ${serverAddress.port}`);
+});
 
 server.timeout = 60000 * 5;
